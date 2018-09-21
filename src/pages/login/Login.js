a1fd6dfbc845591bb8783a56040e6e11e2788396
Santhosh John (UST <U47346@ust-global.com>
2018-09-21 12:35:56 +0530

    Login Page New Layout And a Few Code Fixes in React.

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,6 @@
 import React, { Component, Fragment } from "react";
 import './login.css';
-import { Card } from 'primereact/card';
 import { loginDetails, growlData } from "../../constant";
-import { FaCheck } from 'react-icons/fa';
 import { Growl } from 'primereact/growl';
 
 export class Login extends Component {
@@ -21,11 +19,9 @@ export class Login extends Component {
         this.setState({ loginDetails: loginDetails });
     }
 
+    userNameCheck = () => {
+        const username = this.username.current.value;
 
-    /**
-     * Checks user name is exist in or not
-     */
-    userNameCheck = (username) => {
         const userLoginDetails = this.state.loginDetails.filter(loginDetail => {
             return loginDetail.username === username;
         });
@@ -39,10 +35,16 @@ export class Login extends Component {
         const password = this.password.current.value;
         this.setIsValueEntered();
         const { username, loginDetails } = this.state;
-        if (keyCode === 13 || type === "click" ) {
-            const loginInfo = this.getLoginInfo(username, password, loginDetails);
-            if (loginInfo) {
-                
+        if (keyCode === 13 || type === "click") {
+            if (username && password.length > 0) {
+                let userData = loginDetails.filter((loginDetail => {
+                    return loginDetail.username === username;
+                }))[0];
+                const isLoggedIn = (userData.password === password);
+                const loginInfo = {
+                    username: username,
+                    isLoggedIn: isLoggedIn
+                };
                 this.props.onLogin(loginInfo);
             } else {
                 this.growl.show(growlData.loginError);
@@ -73,57 +75,66 @@ export class Login extends Component {
         return checkClass;
     }
 
-    setIsValueEntered = () =>{
-        if(this.username.current.value.length > 0 && this.password.current.value.length > 0){
+    setIsValueEntered = () => {
+        if (this.username.current.value.length > 0 && this.password.current.value.length > 0) {
             this.setState({
                 isValueEntered: true
             })
-        }else{
+        } else {
             this.setState({
                 isValueEntered: false
             })
         }
     }
-    render() {
 
-        let checkClass = this.getCheckClass();
+    render() {
+        let manageBtnIcon = "btn btn-login btn-cursor-";
+        manageBtnIcon = this.state.isValueEntered ? manageBtnIcon += 'pointer' : manageBtnIcon += 'not-allowed';
+
         return (
             <Fragment>
-                <Growl ref={el => { this.growl = el }} />
-                <div className="login">
-                    <Card
-                        title="Welcome to Library Management System"
-                        subTitle="Login to Proceed"
-                        className="card"
-                    >
-                        <div className="form">
-                            <div className="form-group">
-                                <label htmlFor="username">Username</label>
-                                <FaCheck className={checkClass} />
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="username"
-                                    placeholder="Username"
-                                    ref={this.username}
-                                    onBlur={(input) => { this.userNameCheck(this.username.current.value)}}
-                                />
+                <Growl ref={el => { this.growl = el } } />
+                <section className="login-block">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-4 login-sec">
+                                <h2 className="text-center">Login Now</h2>
+                                <form className="login-form">
+                                    <div className="form-group">
+                                        <label className="badge badge-primary" htmlFor="username">Username</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="username"
+                                            placeholder=""
+                                            ref={this.username}
+                                            onBlur={(input) => { this.userNameCheck() } }
+                                            />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="badge badge-primary" htmlFor="password">Password</label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="password"
+                                            placeholder=""
+                                            ref={this.password}
+                                            onKeyUp={this.formSubmit}
+                                            />
+                                    </div>
+                                    <div className="form-check">
+                                        <label className="form-check-label">
+                                            <input type="checkbox" className="form-check-input" />
+                                            <small>Remember Me (beta) </small>
+                                        </label>
+                                        <button id="submit-btn" type="submit" disabled={!this.state.isValueEntered} className={manageBtnIcon} onClick={this.formSubmit}>Login</button>
+                                    </div>
+                                </form>
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Password</label>
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    id="password"
-                                    placeholder="Password"
-                                    ref={this.password}
-                                    onKeyUp={this.formSubmit}
-                                />
-                            </div>
-                            <button id="submit-btn" disabled={!this.state.isValueEntered} className="btn btn-primary" onClick={this.formSubmit}>Login</button>
+                            <div className="col-md-8 banner-sec"></div>
                         </div>
-                    </Card>
-                </div>
+                    </div>
+                </section>
             </Fragment>
         )
     }
