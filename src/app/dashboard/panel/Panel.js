fa813908de9ae17488c58b8b41dda6b4f866945e
Gurunathan Mouleeshwaran (UST <C86078@ust-global.com>
2018-09-26 18:21:52 +0530

    Variable and classname renaming

--- src/app/dashboard/panel/Panel.js
+++ src/app/dashboard/panel/Panel.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Panel = props => {
-    const {sectionId, sectionHeading, children} = props;
+    const {sectionID, sectionHeading, children} = props;
     return (
-        <section id={sectionId} className="m-2 dashboard-card">
+        <section id={sectionID} className="m-2 panel-card">
             <h4 className="heading">{sectionHeading}</h4>
             {children}
         </section>
