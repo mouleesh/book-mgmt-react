e0d3fad35f1cfffbc56374799d769be10b3461a9
Mouleesh Guru <gggmouleesh@gmail.com>
2018-10-08 08:02:05 +0530

    refactoring folder names

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app/App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'bootswatch/dist/materia/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
 
 registerServiceWorker();
